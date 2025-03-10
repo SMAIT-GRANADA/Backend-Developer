9d9640cf653814b50f2b2aed4397192bb3afa660
Wahyu Pinanda Ginting <Keiwahyu19@gmail.com>
2025-03-11 04:15:46 +0800

    production

--- services/attendanceService.js
+++ services/attendanceService.js
@@ -4,9 +4,9 @@ const { uploadPhotoToGCS, deletePhotoFromGCS } = require('../config/gcs');
 const { Parser } = require('json2csv');
 const ExcelJS = require('exceljs');
 
-const ATTENDANCE_START_HOUR = 0;
+const ATTENDANCE_START_HOUR = 5;
 const ATTENDANCE_LATE_HOUR = 7;
-const ATTENDANCE_LATE_MINUTE = 1;
+const ATTENDANCE_LATE_MINUTE = 20;
 
 const validatePhotoBase64 = (photoBase64) => {
   if (!photoBase64) {
